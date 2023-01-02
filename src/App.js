7e0db846a72d95c89369e1554b8b2c7cbfe2f6fd
H4Nhs <gksgytmd19@kakao.com>
2023-01-03 01:37:25 +0900

    initail commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <div>
+            <form>
+                <input type="text" placeholder="id"></input>
+                <input type="password" placeholder="password"></input>
+                <button>회원가입</button>
+            </form>
+        </div>
+    );
+}
+
+export default App;
